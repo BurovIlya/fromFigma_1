7d2923c72e46fa6285003b39a23403ae2301b8c1
BurovIlya <burovii1981@gmail.com>
2021-01-30 22:57:03 +0300

    volonters finished

--- scripts/volont.js
+++ scripts/volont.js
@@ -1,4 +1,15 @@
-(function() {
+function changeProgs(ind, st){
+       
+       let pos = -st * ind;
+       
+       wrapper.style.left = pos + 'px';
+       let blockHeight = sections[ind].clientHeight;
+       container.style.height = blockHeight + 'px';
+       
+       if(window.pageYOffset > 0) {
+          window.scrollTo(0,0);
+       }
+   }
     
     let fields = document.querySelectorAll('.other-input');
     //alert(fields.length);
@@ -20,7 +31,45 @@
         };
         
         
-        //alert(myInput.name);
+        
     }
     
-})();
+    
+   
+   let container = document.querySelector('#volonters-form > .volonters-form-wrapper');
+   let wrapper = container.firstElementChild;
+   let sections = container.querySelectorAll('.form-part');
+      
+   const step = container.clientWidth;
+   
+   let index = 0;
+   
+   changeProgs(index, step);
+   
+   let forwardElems = wrapper.querySelectorAll('div[data-direct="forward"]');
+   
+   for( let elem of forwardElems ) {
+    
+      elem.addEventListener('click', function() {
+        index++;
+        changeProgs(index, step);
+      })
+    
+   }
+   
+   let backElems = wrapper.querySelectorAll('a[data-direct="back"]');
+   
+   for( let elem of backElems ) {
+    
+      elem.addEventListener('click', function(evt) {
+        
+        evt.preventDefault();
+        index--;
+        changeProgs(index, step);
+      })
+    
+   }
+   
+   
+   
+    
